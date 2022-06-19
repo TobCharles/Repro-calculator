4c049eea834d73eb3473229ead96365ddeeabb15
TobCharles <78859552+TobCharles@users.noreply.github.com>
2022-06-19 23:23:36 +0800

    Update reprocessing calc.py

--- reprocessing calc.py
+++ reprocessing calc.py
@@ -1,151 +1,189 @@
-import time
+print("=============== REPROCESSING CALCULATOR ===============\n")
 
-print("""=============== REPROCESSING CALCULATOR ===============\n
-Step 1: Select Rig type:\n
-1. Tech I Structure rig
-2. Tech II Structure rig
-9. Unsure (calculation assumes un-rigged)
-0. No Structure rig
-""")
+def get_rig_type() -> int:
 
-rig_type = input("please select a rig type:")
-while rig_type not in ['1','2','9','0']:
-    rig_type = input("Invalid, please pick options 1,2,9 or 0:")
-if rig_type == "1":
-    a = int(1)
-elif rig_type == "2":
-    a = int(3)
-else:
-    a = int(0)
-print("Selected option:",rig_type)
-time.sleep(1)
+    print("""Step 1: Select Rig type:\n
+    1. Tech I Structure rig
+    2. Tech II Structure rig
+    9. Unsure (calculation assumes un-rigged)
+    0. No Structure rig
+    """)
 
-print("""Step 2: Select security modifier:\n
-1. High-Security
-2. Low-Security
-3. Null-Security/W-Space
-""")
-if rig_type in ('9','0'):
-    b = int(0)
-    print("\nNOTICE: Structure is unrigged, skipping security modifier...\n")
-    time.sleep(1)
-else:
-    sec_type = input("please select a security type:")
-    while sec_type not in ['1','2','3']:
-        sec_type = input("Invalid, please pick options 1-3:")
-    if sec_type == "2":
-        b = float(0.06)
-    elif sec_type == "3":
-        b = float(0.12)
+    rig_type = int(input("Rig type:"))
+    while rig_type not in [1,2,9,0]:
+        print("Invalid, please pick options 1,2,9 or 0:")
+        rig_type = int(input("Rig type:"))
+    if rig_type == 1:
+        rig_mod = 1
+    elif rig_type == 2:
+        rig_mod = 3
     else:
-        b = int(0)
-    print("Selected option:",sec_type)
-    time.sleep(1)
+        rig_mod = 0
+    
+    return (rig_mod)
+rig = get_rig_type()
 
-print("""Step 3: Select structure type:\n
-1. Athanor structure
-2. Tatara structure
-9. Other structure types
-""")
+def get_sec_type(rig_mod: int) -> float:
 
-strc_type = input("please select a structure type:")
-while strc_type not in ['1','2','9']:
-    strc_type = input("Invalid, please pick options 1-2 or 9:")
-if strc_type == "1":
-    c = float(0.02)
-elif strc_type == "2":
-    c = float(0.055)
-else:
-    c = int(0)
+    if rig_mod == 0:
+        sec_mod = 0
+        print("\nStructure is unrigged, skipping security modifier...")
+    else:
+        print("""Step 2: Select Security Modifier:\n
+            1. High-Security
+            2. Low-Security
+            3. Null-Security/W-Space
+            """)
+        sec_type = int(input("Security Modifier:"))
+        while sec_type not in range(0,4):
+            print("Invalid, please pick options 1-3:")
+            sec_type = int(input("Security value:"))
+        if sec_type == 2:
+            sec_mod = 0.06
+        elif sec_type == 3:
+            sec_mod = 0.12
+        else:
+            sec_mod = 0
+        
+    return (sec_mod)
+sec = get_sec_type(rig)
 
-print("Selected option:",strc_type)
-time.sleep(1)
+def get_strc_type() -> float:
 
-print("""Step 4: Select reprocessing skill level:
-1. Reprocessing I
-2. Reprocessing II
-3. Reprocessing III
-4. Reprocessing IV
-5. Reprocessing V
-0. No Reprocessing skill
-""")
+    print("""Step 3: Select structure type:\n
+    1. Athanor structure
+    2. Tatara structure
+    9. Other structure types
+    """)
 
-rep_skill = int(input("please select your reprocessing skill:"))
-while rep_skill not in range(0,6):
-    rep_skill = int(input("Invalid, please pick options 1-5 or 0:"))
-else:
-    d = rep_skill
+    strc_type = int(input("Structure type:"))
+    while strc_type not in [1,2,9]:
+        print("Invalid, please pick options 1,2 or 9:")
+        strc_type = int(input("Structure type:"))
+    if strc_type == 1:
+        strc_mod = 0.02
+    elif strc_type == 2:
+        strc_mod = 0.055
+    else:
+        strc_mod = 0.0
 
-print("Selected option:",rep_skill)
-time.sleep(1)
+    return strc_mod
+strct = get_strc_type()
 
-print("""Step 5: Select reprocessing efficiency skill level:
-1. Reprocessing Efficiency I
-2. Reprocessing Efficiency II
-3. Reprocessing Efficiency III
-4. Reprocessing Efficiency IV
-5. Reprocessing Efficiency V
-0. No Reprocessing Efficiency skill
-""")
+def get_rep_skill() -> int:
 
-eff_skill = int(input("please select your reprocessing efficiency skill:"))
-while eff_skill not in range(0,6):
-    eff_skill = int(input("Invalid, please pick options 1-5 or 0:"))
-else:
-    e = eff_skill
+    print("""Step 4: Select reprocessing skill level:
+    1. Reprocessing I
+    2. Reprocessing II
+    3. Reprocessing III
+    4. Reprocessing IV
+    5. Reprocessing V
+    0. No Reprocessing skill
+    """)
 
-print("Selected option:",eff_skill)
-time.sleep(1)
+    rep_skill = int(input("Reprocessing Skill:"))
+    while rep_skill not in range(0,6):
+        print("Invalid, please pick options 1-5 or 0:")
+        rep_skill = int(input("Reprocessing Skill:"))
+    else:
+        rep_mod = rep_skill
 
-print("""Step 6: Select specific ore skill level:
-1. <Ore> Reprocessing I
-2. <Ore> Reprocessing II
-3. <Ore> Reprocessing III
-4. <Ore> Reprocessing IV
-5. <Ore> Reprocessing V
-0. No <Ore> Reprocessing skill
-""")
+    return (rep_mod)
+rep = get_rep_skill()
 
-ore_skill = int(input("please select your specific ore skill:"))
-while ore_skill not in range(0,6):
-    ore_skill = int(input("Invalid, please pick options 1-5 or 0:"))
-else:
-    f = ore_skill
+def get_eff_skill() -> int:
 
-print("Selected option:",ore_skill)
-time.sleep(1)
+    print("""Step 5: Select reprocessing efficiency skill level:
+    1. Reprocessing Efficiency I
+    2. Reprocessing Efficiency II
+    3. Reprocessing Efficiency III
+    4. Reprocessing Efficiency IV
+    5. Reprocessing Efficiency V
+    0. No Reprocessing Efficiency skill
+    """)
 
-print("""Step 7: Select Implants:
-1. RX-801 Implant
-2. RX-802 Implant
-3. RX-804 Implant
-0. No Implants""")
+    eff_skill = int(input("Reprocessing Skill:"))
+    while eff_skill not in range(0,6):
+        print("Invalid, please pick options 1-5 or 0:")
+        eff_skill = int(input("Reprocessing Skill:"))
+    else:
+        eff_mod = eff_skill
 
-implants = input("please select implants:")
-while implants not in ['1','2','3','0']:
-    implants = input("Invalid, please pick options 1-3 or 0:")
-if implants == "1":
-    g = float(0.01)
-elif implants == "2":
-    g = float(0.02)
-elif implants == "3":
-    g = float(0.04)
-else:
-    g = int(0)
+    return (eff_mod)
+eff = get_eff_skill()
 
-print("Selected option:",implants)
-time.sleep(1)
+def get_ore_skill() -> int:
 
-print('You have selected options:\nRig Option:',a,'\n''Security Option:',b,'\n''Structure Option:',c,'\n''Reprocessing Skill:',d,'\n''Efficiency Skill:',e,'\n''Specific Skill:',f,'\n''Implant Option:',g,'\n''.....Calculating.....')
-time.sleep(1)
+    print("""Step 6: Select specific ore skill level:
+    1. <Ore> Reprocessing I
+    2. <Ore> Reprocessing II
+    3. <Ore> Reprocessing III
+    4. <Ore> Reprocessing IV
+    5. <Ore> Reprocessing V
+    0. No <Ore> Reprocessing skill
+    """)
 
-repo_yield = ((50+a)*(1+b))*(1+c)*(1+(0.03*d))*(1+(0.03*e))*(1+(0.02*f))*(1+g)
-repo_unrig = (50+a)*(1+c)*(1+(0.03*d))*(1+(0.03*e))*(1+(0.02*f))*(1+g)
-yield_round = round(float(repo_yield),1)
-unrig_round = round(float(repo_unrig),1)
+    ore_skill = int(input("Specific Ore Skill:"))
+    while ore_skill not in range(0,6):
+        print("Invalid, please pick options 1-5 or 0:")
+        ore_skill = int(input("Specific Ore Skill:"))
+    else:
+        ore_mod = ore_skill
 
-if rig_type in ('9','0'):
-    print("Your reprocessing yield is roughly:",unrig_round,"%","\nNOTE: structure is unrigged")
-else:
-    print("Your reprocessing yield is roughly:",yield_round,"%")
+    return (ore_mod)
+ore = get_ore_skill()
 
+def get_imp_type() -> float:
+
+    print("""Step 7: Select Implants:
+    1. RX-801 Implant
+    2. RX-802 Implant
+    3. RX-804 Implant
+    0. No Implants
+    """)
+
+    imp_type = int(input("Implant Type:"))
+    while imp_type not in range(0,4):
+        print("Invalid, please pick options 1-3 or 0:")
+        imp_type = int(input("Implant Type:"))
+    if imp_type == 1:
+        imp_mod = 0.01
+    elif imp_type == 2:
+        imp_mod = 0.02
+    elif imp_type == 3:
+        imp_mod = 0.04
+    else:
+        imp_mod = 0.0
+
+    return (imp_mod)
+impl = get_imp_type()
+
+print(
+    "You have selected options:\nRig Type:",
+    rig,
+    "\n" "Security Type:",
+    sec,
+    "\n" "Structure Type:",
+    strct,
+    "\n" "Reprocessing Skill:",
+    rep,
+    "\n" "Efficiency Skill:",
+    eff,
+    "\n" "Specific Skill:",
+    ore,
+    "\n" "Implant Type:",
+    impl
+)
+
+def calc_repro_yield():
+    return (
+        ((50 + rig) * (1 + sec))
+        * (1 + strct)
+        * (1 + (0.03 * rep))
+        * (1 + (0.03 * eff))
+        * (1 + (0.02 * ore))
+        * (1 + impl)
+    )
+
+repro_norm = round(calc_repro_yield(),1)
+print("\nYield:",repro_norm,"%")
